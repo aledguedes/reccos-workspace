6a4510d7b12f7a65d615c4e8523467ff8e7b8cf0
Alexandre Guedes <alexandredguedes@gmail.com>
2025-03-14 16:38:37 -0300

    sidebar compoenent

--- projects/reccos-v2/src/app/shared/components/main-layout/main-layout.component.ts
+++ projects/reccos-v2/src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    SidebarComponent,
+    NavbarComponent,
+  ],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
   // Variável para controlar a exibição da sidebar em dispositivos móveis
-  isSidebarOpen = true;
+  isSidebarOpen = false; // Inicializa fechada para dispositivos móveis
   // Variável para controlar o estado de expansão da sidebar
   isSidebarExpanded = true;
 
